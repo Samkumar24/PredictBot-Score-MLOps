46f905bfd069bf9964f49212c4fb18322f1d3574
Sam Kumar <samkumarr24@gmail.com>
2026-06-15 16:08:37 +0530

    Model evaluation done

--- main.py
+++ main.py
@@ -3,8 +3,6 @@ from src.predictor_bot_score.components._02_data_validation  import Data_validat
 from src.predictor_bot_score.components._03_data_transformation  import Data_transformation
 from src.predictor_bot_score.components._04_feature_enginerring import Feature_engineering
 from src.predictor_bot_score.components._05_model_building import Model_Building
-
-from src.predictor_bot_score.entity import (Data_injestion_config, DataValidationConfig ,DataTransformationConfig)
 from src.predictor_bot_score.config.configuration import (yaml_configruation)
 from src.predictor_bot_score.logger import logger
 from src.predictor_bot_score.components._01_data_injestion  import Data_injestion
